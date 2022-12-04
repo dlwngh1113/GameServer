0e4baea8557566f1487745e41839fd795ec86b13
주호 이 <dlwngh1113@naver.com>
2022-12-04 22:17:55 +0900

    Sector 클래스 추가

--- server/2020_IOCP_SERVER/Sector.cpp
+++ server/2020_IOCP_SERVER/Sector.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "Sector.h"
+
+Sector::Sector(int nX, int nY, int nWidth, int nHeight) : m_nX{ nX }, m_nY{ nY }, m_nWidth{ nWidth }, m_nHeight{ nHeight }
+{
+}
+
+Sector::~Sector()
+{
+	m_users.clear();
+}
+
+void Sector::AddUser(User* user)
+{
+	m_users.insert(user);
+}
+
+void Sector::RemoveUser(User* user)
+{
+	if (m_users.count(user) != 0)
+		m_users.erase(user);
+}
--- server/2020_IOCP_SERVER/Sector.h
+++ server/2020_IOCP_SERVER/Sector.h
@@ -4,11 +4,13 @@
 class Sector
 {
 private:
+	int m_nX{ 0 }, m_nY{ 0 }, m_nWidth{ 0 }, m_nHeight{ 0 };
 	std::unordered_set<User*> m_users;
 
 public:
+	Sector(int nX, int nY, int nWidth, int nHeight);
+	virtual ~Sector();
 
 	void AddUser(User* user);
 	void RemoveUser(User* user);
-};
-
+};	
