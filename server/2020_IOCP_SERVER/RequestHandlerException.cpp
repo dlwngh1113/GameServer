50e0cf30c9549d8950ee4277969c9e9e8f08d1cf
주호 이 <dlwngh1113@naver.com>
2023-06-03 23:54:31 +0900

    feat: handler 로그 포맷 지정

--- server/2020_IOCP_SERVER/RequestHandlerException.cpp
+++ server/2020_IOCP_SERVER/RequestHandlerException.cpp
@@ -1,2 +1,20 @@
 #include "stdafx.h"
 #include "RequestHandlerException.h"
+
+RequestHandlerException::RequestHandlerException() : m_sErrorLocation{},  exception()
+{
+}
+
+RequestHandlerException::RequestHandlerException(const char* sMessage, const char* errorLocation = __FILE__ + __LINE__) : exception(sMessage)
+{
+}
+
+RequestHandlerException::~RequestHandlerException()
+{
+}
+
+const char* RequestHandlerException::what() const
+{
+    
+    return nullptr;
+}
--- server/2020_IOCP_SERVER/RequestHandlerException.h
+++ server/2020_IOCP_SERVER/RequestHandlerException.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#define ERROR_FORMAT __FILE__ + __LINE__
+
 class RequestHandlerException : public std::exception
 {
+	std::string m_sErrorLocation;
+public:
+	explicit RequestHandlerException();
+	explicit RequestHandlerException(const char* sMessage, const char* sErrorLocation = ERROR_FORMAT);
+	virtual ~RequestHandlerException();
+
+	_NODISCARD virtual const char* what() const override;
 };
