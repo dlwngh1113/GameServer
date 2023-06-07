e8234704e35f301b875f4c5886e8b4dff05614aa
주호 이 <dlwngh1113@naver.com>
2023-06-07 23:16:11 +0900

    feat: player 관련 메소드 추가

--- server/Client/Scene.cpp
+++ server/Client/Scene.cpp
@@ -23,3 +23,25 @@ void Scene::Update()
 void Scene::OnEvent(SDL_Event& evt)
 {
 }
+
+void Scene::AddOtherPlayer(OtherPlayer* otherPlayer)
+{
+	m_otherPlayers.push_back(otherPlayer);
+}
+
+OtherPlayer* Scene::GetOtherPlayer(int nId)
+{
+	OtherPlayer* value{ nullptr };
+
+	for (const auto& player : m_otherPlayers)
+		;
+
+	return value;
+}
+
+void Scene::RemoveOtherPlayer(OtherPlayer* otherPlayer)
+{
+	for (auto i = m_otherPlayers.begin(); i != m_otherPlayers.end(); ++i)
+		if (*i == otherPlayer)
+			m_otherPlayers.erase(i);
+}
--- server/Client/Scene.h
+++ server/Client/Scene.h
@@ -17,4 +17,12 @@ public:
 	virtual void Render(SDL_Renderer* renderer);
 	virtual void Update();
 	virtual void OnEvent(SDL_Event& evt);
+
+public:
+	void SetPlayer(MyPlayer* player) { m_myPlayer = player; }
+	const MyPlayer* GetPlayer() const { return m_myPlayer; }
+
+	void AddOtherPlayer(OtherPlayer* otherPlayer);
+	OtherPlayer* GetOtherPlayer(int nId);
+	void RemoveOtherPlayer(OtherPlayer* otherPlayer);
 };
