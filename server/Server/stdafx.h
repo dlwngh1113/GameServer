d38e56d691ea2c334845df23fa6def04d3c85b43
주호 이 <dlwngh1113@naver.com>
2024-02-16 21:53:42 +0900

    resource: add mysql connector

--- server/Server/stdafx.h
+++ server/Server/stdafx.h
@@ -27,6 +27,11 @@ extern "C" {
 #include <boost/uuid/random_generator.hpp>
 #include <boost/array.hpp>
 
+// mysql
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/resultset.h>
+
 #pragma comment(lib, "lua54.lib")
+#pragma comment(lib, "mysqlcppconn.lib")
 
 #include "../Common/Common.hpp"
