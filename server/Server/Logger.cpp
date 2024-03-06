85b7d57ebdfaa9ef140e3201533b58c2389a68a8
주호 이 <dlwngh1113@naver.com>
2024-03-06 21:59:33 +0900

    feat: logger 구조 변경

--- server/Server/Logger.cpp
+++ server/Server/Logger.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "Logger.h"
+
+void Logger::Log(const char* message)
+{
+	Log(message);
+}
+
+void Logger::Log(const string& message)
+{
+	m_messages.push(message);
+}
--- server/Server/Logger.h
+++ server/Server/Logger.h
@@ -2,4 +2,18 @@
 
 class Logger
 {
+private:
+	queue<string> m_messages;
+
+public:
+	void Log(const char* message);
+	void Log(const string& message);
+
+	// Static member variables
+private:
+	static Logger s_instance;
+
+	// Static member functions
+public:
+	static Logger& instance() { return s_instance; }
 };
