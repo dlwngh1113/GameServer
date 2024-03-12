5b4b85af29abc7a2085e63d3e7866497de4a02ed
주호 이 <dlwngh1113@naver.com>
2024-03-12 22:10:01 +0900

    feat: add packet library

---
+++ server/CommonLib/Packet.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#pragma pack(push, 1)
+struct Header
+{
+	short type;
+	short size;
+};
+#pragma pack(pop)
+
+class Packet
+{
+protected:
+	short m_type;
+	vector<unsigned char> m_buffer;
+
+public:
+	void Serialize();
+
+	template <typename T>
+	Packet& operator<<(const T& val);
+
+	template <>
+	Packet& operator<<<string>(const string& val);
+
+	template <typename T>
+	Packet& operator>>(T& val);
+
+	template <>
+	Packet& operator>><string>(string& val);
+};
