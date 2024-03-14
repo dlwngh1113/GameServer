fc9f2cff6c59c905643d3074585955acea3eac14
주호 이 <dlwngh1113@naver.com>
2024-03-14 21:44:35 +0900

    feat: 함수 내부 로직 수정

--- server/CommonLib/Packet.cpp
+++ server/CommonLib/Packet.cpp
@@ -17,30 +17,35 @@ namespace Common
 	template <typename T>
 	Packet& Packet::operator<<(const T& val)
 	{
-		short size = sizeof(val);
-		m_buffer.insert(m_buffer.end(), size, &val);
-
-		m_offset += size;
+		const unsigned char* ptr = reinterpret_cast<const unsigned char*>(&val);
+		m_buffer.insert(m_buffer.end(), ptr, ptr + sizeof(val));
 	}
 
 	template <>
 	Packet& Packet::operator<<<string>(const string& val)
 	{
-		short size = val.size();
+		short size = static_cast<short>(val.size());
 		this->operator<<(size);
 
-		m_buffer.insert(m_buffer.end(), size, &val[0]);
+		m_buffer.insert(m_buffer.end(), val.begin(), val.end());
 	}
 
 	template <typename T>
 	Packet& Packet::operator>>(T& val)
 	{
-
+		memcpy_s(&val, sizeof(T), m_buffer.data() + m_offset, sizeof(T));
+		m_offset += sizeof(T);
 	}
 
 	template <>
 	Packet& Packet::operator>><string>(string& val)
 	{
+		short size;
+		this->operator>>(size);
 
+		val.resize(size);
+		memcpy_s(&val[0], size, (m_buffer.data() + m_offset), size);
+
+		m_offset += size;
 	}
 }
