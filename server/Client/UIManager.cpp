a083bab817626d23f5d3f2d478f58136d757dc99
LeeJuho <dlwngh1113@naver.com>
2024-05-17 23:47:42 +0900

    feat: GetUI 함수 추가

--- server/Client/UIManager.cpp
+++ server/Client/UIManager.cpp
@@ -1,2 +1,11 @@
 #include "pch.h"
 #include "UIManager.h"
+
+namespace ClientFramework
+{
+	UIBase* UIManager::GetUI(UIType type)
+	{
+		auto it = m_uiControls.find(type);
+		return it != m_uiControls.end() ? it->second.get() : nullptr;
+	}
+}
--- server/Client/UIManager.h
+++ server/Client/UIManager.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "Label.h"
 
-class UIManager
+namespace ClientFramework
 {
-};
+	enum class UIType
+	{
+
+	};
+
+	class UIManager
+	{
+	private:
+		std::unordered_map<UIType, std::unique_ptr<UIBase>> m_uiControls;
+
+	public:
+		UIBase* GetUI(UIType type);
+	};
+}
