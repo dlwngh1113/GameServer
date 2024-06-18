b98d517c3b26831438cc6b945e63277b1ca6f5ca
LeeJuho <dlwngh1113@naver.com>
2024-06-18 23:14:55 +0900

    feat: login 구현

--- server/Server/LoginCommandHandler.cpp
+++ server/Server/LoginCommandHandler.cpp
@@ -2,6 +2,8 @@
 #include "LoginCommandHandler.h"
 #include "DataBase.h"
 #include "Logger.h"
+#include "CServer.h"
+#include "User.h"
 
 void LoginCommandHandler::HandleRequest()
 {
@@ -9,7 +11,7 @@ void LoginCommandHandler::HandleRequest()
 	Common::PacketStream ps(m_data.data(), m_data.size());
 	body.Deserialize(ps);
 
-	std::string id(body.id);
+	std::string id(body.userId);
 	std::string password(body.password);
 
 	if (id.empty() || id.size() < 12)
@@ -24,5 +26,18 @@ void LoginCommandHandler::HandleRequest()
 		return;
 	}
 
+	sql::PreparedStatement* stmt(Core::DataBase::instance().GetConnection()->prepareStatement("CALL GetUser(?,?)"));
+	sql::ResultSet* result(stmt->executeQuery());
 
+	float x(result->getDouble("x"));
+	float y(result->getDouble("y"));
+
+	Common::LoginResponseBody resBody;
+	resBody.id = body.id;
+	resBody.x = x;
+	resBody.y = y;
+	
+	std::shared_ptr<User> user = CServer::instance().GetUser(m_peer->id());
+	if (user)
+		user->SendPacket(&resBody);
 }
