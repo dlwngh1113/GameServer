24454522b62615a26ae065c75901c4249e6aaac8
LeeJuho <dlwngh1113@naver.com>
2024-06-26 22:06:33 +0900

    feat: error 함수 추가

--- server/Server/BaseCommandHandler.cpp
+++ server/Server/BaseCommandHandler.cpp
@@ -19,11 +19,24 @@ namespace Core
 		memcpy_s(m_data.data(), m_data.size(), data, size);
 	}
 
-	void BaseCommandHandler::SendResponse(std::shared_ptr<Common::Packet> packet)
+	void BaseCommandHandler::SendResponse(std::shared_ptr<Common::ResponseBody> response)
 	{
 		Common::PacketStream ps;
-		std::string data = packet->Serialize(ps);
+		response->code = kCode_Success;
+		std::string data = response->Serialize(ps);
 
 		m_peer->SendData(data);
+	}
+
+	void BaseCommandHandler::Error(short code, const std::string& message)
+	{
+		//Common::PacketStream ps;
+		//std::shared_ptr<Common::ResponseBody> res = std::make_shared<Common::ResponseBody>();
+		//res->code = code;
+		//res->message = message;
+
+		//std::string data = res->Serialize(ps);
+
+		//m_peer->SendData(data);
 	}
 }
